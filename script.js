2b1141eb9b9b2e9b190a38b616a53fd3e61e64dd
Yuvaraj9607 <yuvi9607@gmail.com>
2026-06-28 17:19:34 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 async function loadMenu() {
-  const response = await fetch('/menu');
-  const menu = await response.json();
-  let html = `<table><tr><th>ID</th><th>Item Name</th><th>Price</th></tr>`;
-  menu.forEach(item => {
-    html += `<tr><td>${item.id}</td><td>${item.item_name}</td><td>₹${item.price}</td></tr>`;
-  });
-  html += `</table>`;
-  document.getElementById('out').innerHTML = html;
+
+    const response = await fetch('/menu');
+    const menu = await response.json();
+
+    let html = `
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Item Name</th>
+                <th>Price</th>
+            </tr>
+    `;
+
+    menu.forEach(item => {
+        html += `
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.item_name}</td>
+                <td>₹${item.price}</td>
+            </tr>
+        `;
+    });
+
+    html += `</table>`;
+
+    document.getElementById("out").innerHTML = html;
 }
